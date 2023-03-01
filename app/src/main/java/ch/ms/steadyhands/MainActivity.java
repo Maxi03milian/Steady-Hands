41997608dc8506fd47f36c2fae49162e3878a553
Schmid Maximilian, GHR-OSA-NEX-31 <Maxi03milian@gmail.com>
2023-03-01 16:07:00 +0100

    feat: changed default color

--- app/src/main/java/ch/ms/steadyhands/MainActivity.java
+++ app/src/main/java/ch/ms/steadyhands/MainActivity.java
@@ -2,7 +2,9 @@ package ch.ms.steadyhands;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get button with id "startButton"
+        Button startButton = (Button) findViewById(R.id.startButton);
+        //set onClickListener
+        startButton.setOnClickListener(v -> startGame());
+
+
+    }
+
+    private void startGame() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
